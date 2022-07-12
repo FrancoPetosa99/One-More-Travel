2e8f9500c5084daaf6242c886d686b12730f3ff2
Franco Petosa Ayala <84604362+FrancoPetosa99@users.noreply.github.com>
2022-07-12 13:31:43 -0300

    updates

--- scripts/loading.js
+++ scripts/loading.js
@@ -4,21 +4,28 @@
     purpose: show a loading spinner until the page is completed loaded. Gives a better UX/UI.
 */
 
-function loading() {
-  const body = document.querySelector("html");
+const body = document.querySelector("html");
 
-  //build-up the loading (Html element)
-  const loadingModal = document.createElement("div");
-  loadingModal.className = "loadingModal";
-  loadingModal.innerHTML = `<div id="spinner"></div>`;
+//build-up the loading (Html element)
+const loadingModal = document.createElement("div");
+loadingModal.className = "loadingModal";
+loadingModal.innerHTML = `<div id="spinner"></div>`;
 
-  body.appendChild(loadingModal);
+function loading(setLoading) {
 
-  //Once the page is completed loaded, it removes the loading (Html element)
-  window.addEventListener("DOMContentLoaded", () => {
-    setTimeout(() => {
-      body.removeChild(loadingModal);
-    }, 1000);
-  });
+  if(setLoading){
+    
+    body.appendChild(loadingModal);
+    return
+  }else{
+
+    //Once the page is completed loaded, it removes the loading (Html element)
+    window.addEventListener("DOMContentLoaded", () => {
+      setTimeout(() => {
+        body.removeChild(loadingModal);
+      }, 1000);
+    });
+
+  }
 }
-loading();
+loading(true);
